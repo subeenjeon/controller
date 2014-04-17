38784bf75678ae0aaad7ec910f6661da58d014f5
subeenjeon <powersubeen@naver.com>
2014-04-18 05:50:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 $(document).ready(function(){
-	$('#up-btn').click(function(){
-		$('#player').animate({top: "-=60px"}, 'fast');
-	});
-	$('#down-btn').click(function(){
-		$('#player').animate(top: "+=60px"}, 'fast');
-	});
-	$('#left-btn').click(function(){
-		$('#player').animate({left:"-=60px"}, 'fast');
-	});
-	$('#right-btn').click(function(){
-		$('#player').animate({left:"+=60px"}, 'fast');
-	});
-	$('#in-btn').click(function(){
-		$('#player').fadeIn('slow');
-	});
-	$('#out-btn').click(function(){
-		$('#player').fadeOut('slow');
-	});
+   $('#up-btn').click(function() {
+    	$('#player').animate({top: "-=10px"}, 'fast');
+    });
+    $('#down-btn').click(function() {
+    	$('#player').animate({top: "+=10px"}, 'fast');
+    });
+    $('#left-btn').click(function() {
+    	$('#player').animate({left: "-=10px"}, 'fast');
+    });
+    $('#right-btn').click(function() {
+    	$('#player').animate({left: "+=10px"}, 'fast');
+    });
+    $('#in-btn').click(function() {
+    	$('#player').fadeIn('fast');
+    });
+    $('#out-btn').click(function() {
+    	$('#player').fadeOut('fast');
+    });
+    
 	$('#player').hover(function (){
 		$('#player').effect('bounce', {times:5}, 700);
 	});
