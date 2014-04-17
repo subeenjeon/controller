f181edff01313703c0e796e0fc120d60a0d17127
subeenjeon <powersubeen@naver.com>
2014-04-18 04:26:40 +0900

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ $(document).ready(function(){
     $('#out-btn').click(function() {
     	$('#player').fadeOut('slow');
     });
-    $('#player').click(function(){
+    $('div').click(function(){
       $('#player').effect('bounce', {times:5}, 700);
     });
 });
