0588dbee0cd9763b2f7c6129e2950db4a13ac979
subeenjeon <powersubeen@naver.com>
2014-04-18 05:41:52 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 $(document).ready(funtion(){
 	$('#up-bth').click(function)(){
-		$('#player').animate({top:"+=60px"}, 'fast');
+		$('#player').animate({top:"-=60px"}, 'fast');
 	});
 	$('#down-bth').click(function (){
-		$('#player').animate({down:"-=60px"}, 'fast');
+		$('#player').animate(top:"+=60px"}, 'fast');
 	});
 	$('#left-bth').click(function (){
 		$('#player').animate({left:"-=60px"}, 'fast');
+	});
 	$('#right-bth').click(function (){
-		$('#player').animate({right:"-=60px"}, 'fast');
+		$('#player').animate({left:"+=60px"}, 'fast');
 	});
 	$('#in-bth').click(function (){
 		$('#player').fadeIn ('slow');
